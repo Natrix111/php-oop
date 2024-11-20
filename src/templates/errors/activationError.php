158b4a145d336efb396eb71e119bd77e1650d000
Natrix111 <dmitryk414@gmail.com>
2024-11-20 14:23:55 +0300

    level-3

---
+++ src/templates/errors/activationError.php
@@ -0,0 +1,7 @@
+<?php include __DIR__ . '/../header.php'; ?>
+<div>
+    <p>Не удалось активировать пользователя(</p>
+    <br>
+    <?= $error ?>
+</div>
+<?php include __DIR__ . '/../footer.php'; ?>
